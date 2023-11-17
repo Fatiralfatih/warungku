3e74f5638e331b2e2b6a94f4da8f71f143de84df
Fatiralfatih <137702546+Fatiralfatih@users.noreply.github.com>
2023-11-17 18:37:43 +0700

    aku ingin mencoba push commit yang ada tapi aku bingung dadadad

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::group(["middleware" => ["auth"]], function () {
 // dada didi dudu
 // push reng
 // push reng ke 2
+// mudo loro
     Route::middleware(['pelayan'])->prefix('pelayan')->group(function () {
         // data makanan for pelayan
         Route::get('menu', [PelayanMenuController::class, 'index'])->name('pelayan.menu');
